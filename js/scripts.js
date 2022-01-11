9b800c9400a45ecf8f6df3771bad74a8195f2edf
Juan Pablo Simon <jsimon0301@gmail.com>
2022-01-11 19:41:13 -0300

    [MODIFIED] modificacion de stock y agregado de contacto.

--- js/scripts.js
+++ js/scripts.js
@@ -16,25 +16,25 @@ function Planta(nombre, precio,imagen) {
 
 const producto1 = new Planta("Rosario", "$200 - $250 ", "rosario")
 const producto2 = new Planta("Planta Fantasma", "$250 - $300", "PlantaFantasma")
-const producto3 = new Planta("Planta cebra", "$300 - $500", "plantaCebra")
-const producto4 = new Planta("Aeonium Kiwi", "$200 - $350", "aeoniumKiwi")
+const producto3 = new Planta("Planta cebra", "$300", "plantaCebra")
+const producto4 = new Planta("Aeonium Kiwi", "$200 - $400", "aeoniumKiwi")
 const producto5 = new Planta("Echeveria runyonii", "$250 - $350", "echeveriaRunyonii")
-const producto6 = new Planta("Haworthia Retusa", "$150 - $250", "haworthiaRetusa")
+// const producto6 = new Planta("Haworthia Retusa", "$150 - $250", "haworthiaRetusa")
 const producto7 = new Planta("Cordón de San Jose", "$450", "cordonSanJose")
 const producto8 = new Planta("Aeonium Haworthii", "$250 - $350", "aeoniumHaworthii")
 const producto9 = new Planta("Graptosedum Francesco Baldi", "$200 - $300", "graptosedumFrancesco")
-const producto10 = new Planta("  -  ", "$250 - $350", "desconocida1")
-const producto11 = new Planta("  -  ", "$250", "desconocida2")
+// const producto10 = new Planta("  -  ", "$250 - $350", "desconocida1")
+const producto11 = new Planta("Cubremacetas1", "$250", "desconocida2")
 const producto12 = new Planta("  -  ", "$200", "desconocida3")
-const producto13 = new Planta("  -  ", "$250", "desconocida4")
-const producto14 = new Planta("  -  ", "$350", "desconocida5")
+const producto13 = new Planta("Cubremacetas2", "$300", "desconocida4")
+const producto14 = new Planta("Cubremacetas4", "$350", "desconocida5")
 const producto15 = new Planta("Aeonium Haworthii - Grande", "$500", "aeoniumHaworthii2")
-const producto16 = new Planta("  -  ", "$280", "cactus1")
-const producto17 = new Planta("  -  ", "$280", "cactus2")
-const producto18 = new Planta("Echeveria 'Topsy Turvy' ", "$350", "echeveriaTopsyTurvy")
-const producto19 = new Planta("Haworthia Reinwardtii", "$350", "haworthiaReinwardtii")
+const producto16 = new Planta("Cactus", "$280", "cactus1")
+const producto17 = new Planta("Cactus", "$280", "cactus2")
+// const producto18 = new Planta("Echeveria 'Topsy Turvy' ", "$350", "echeveriaTopsyTurvy")
+// const producto19 = new Planta("Haworthia Reinwardtii", "$350", "haworthiaReinwardtii")
 
-const productos = [producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8, producto9, producto10, producto11, producto12,producto13, producto14,producto15, producto16, producto17, producto18, producto19]
+const productos = [producto1, producto2, producto3, producto4, producto5, producto7, producto8, producto9, producto11, producto12,producto13, producto14,producto15, producto16, producto17]
 
 for (const producto of productos) {
     let contenedor = document.createElement("div");
